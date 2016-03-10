07df49277c7252f730124de7374a37a3615576b5
Brett Warminski <bwarminski@gmail.com>
2016-03-10 08:30:32 -0500

    Work in progress

--- zero_netcat/zero_netcat.py
+++ zero_netcat/zero_netcat.py
@@ -2,6 +2,9 @@
 
 import six, sys, argparse, zmq, codecs, os, zmq.devices, time
 from six.moves import range
+import select
+import ctypes
+from ctypes import *
 
 # zeronc <bind|connect> <type> <address>
 
@@ -14,6 +17,34 @@ SOCKET_FROM_STR = {
 
 SENDS = ['pub', 'push']
 RECEIVES = ['sub', 'pull']
+
+LIBC = CDLL(None)
+
+# Shit just got real
+
+
+# File API.
+#
+# A FILE_ptr type is used instead of c_void_p because technically a pointer
+# to structure can have a different size or alignment to a void pointer.
+#
+# Note that the file api may change.
+#
+
+class FILE(ctypes.Structure):
+    pass
+FILE_ptr = ctypes.POINTER(FILE)
+
+PyFile_FromFile = ctypes.pythonapi.PyFile_FromFile
+PyFile_FromFile.restype = ctypes.py_object
+PyFile_FromFile.argtypes = [FILE_ptr,
+                            ctypes.c_char_p,
+                            ctypes.c_char_p,
+                            ctypes.CFUNCTYPE(ctypes.c_int, FILE_ptr)]
+
+PyFile_AsFile = ctypes.pythonapi.PyFile_AsFile
+PyFile_AsFile.restype = FILE_ptr
+PyFile_AsFile.argtypes = [ctypes.py_object]
 
 class AddressAction(argparse.Action):
     def __call__(self, parent_parser, namespace, values, option_string=None):
@@ -31,45 +62,39 @@ def main(args=sys.argv[1:]):
     parser.add_argument('--send-hwm', type=int, default=1000)
     parser.add_argument('--receive-hwm', type=int, default=1000)
     parser.add_argument('--subscribe', action='append')
-    parser.add_argument('--topic', default='default_topic')
+    parser.add_argument('--topic', default=None)
     parser.add_argument('--buffer-size', type=int, default=100)
-    parser.add_argument('--output', default='')
     parser.add_argument('address', nargs='+', action=AddressAction)
     options = parser.parse_args(args)
     context = zmq.Context.instance(options.io_threads)
     context.set(zmq.IPV6, 1 if options.ipv6 else 0)
     send(context, options) if options.address[0].type in SENDS else receive(context, options)
 
-
 def send(context, options):
-    bcast = context.socket(zmq.PUB)
-    bcast.bind('inproc://bcast')
+    if len(options.address) != 1:
+        raise "Only one output address is supported"
 
-    for addr in options.address:
-        proxy = zmq.devices.ThreadProxy(zmq.SUB, SOCKET_FROM_STR[addr.type])
-        proxy.setsockopt_out(zmq.SNDHWM, options.send_hwm)
-        if addr.bind_connect == 'bind':
-            proxy.bind_out(addr.address)
-        else:
-            proxy.connect_out(addr.address)
-        proxy.setsockopt_in(zmq.SUBSCRIBE, six.u('').encode('utf-8'))
-        proxy.connect_in('inproc://bcast')
-        proxy.start()
+    addr = options.address[0]
+    socket = context.socket(SOCKET_FROM_STR[addr.type])
+    socket.set(zmq.SNDHWM, options.send_hwm)
+    if addr.bind_connect == 'bind':
+        socket.bind(addr.address)
+    else:
+        socket.connect(addr.address)
 
     input = codecs.getreader('utf-8')(os.fdopen(sys.stdin.fileno(), 'r', options.buffer_size))
     while True:
         line = input.readline()
         if not line: break
-        bcast.send_multipart([options.topic.encode('utf-8'), line.rstrip().encode('utf-8')])
+        if options.topic == None:
+            socket.send(line.rstrip().encode('utf-8'))
+        else:
+            socket.send_multipart([options.topic.encode('utf-8'), line.rstrip().encode('utf-8')])
 
 
 def receive(context, options):
     input = context.socket(zmq.PULL)
     input.bind('inproc://input')
-    output = sys.stdout.fileno()
-    fdout = len(options.output) > 0
-    if fdout:
-        output = os.open(options.output, os.O_WRONLY)
 
     for addr in options.address:
         proxy = zmq.devices.ThreadProxy(SOCKET_FROM_STR[addr.type], zmq.PUSH)
@@ -88,15 +113,14 @@ def receive(context, options):
         proxy.connect_out('inproc://input')
         proxy.start()
 
-    if fdout:
-        while True:
-            input.recv_string() #topic
-            os.write(output, input.recv_string().encode('utf-8'))
-            os.write(output, '\n'.encode('utf-8'))
-    else:
-        while True:
-            input.recv_string() #topic
-            six.print_(input.recv_string().encode('utf-8'), flush=True)
+    poller = select.poll()
+    poller.register(sys.stdout, select.POLLOUT)
+    while True:
+        poller.poll()
+        input.recv_string() #topic
+        buf = input.recv_string().encode('utf-8')
+
+        six.print_(input.recv_string().encode('utf-8'), flush=True)
 
 
 
